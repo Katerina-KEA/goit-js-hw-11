119f5d1bc696d386196409137404ef69488bf103
Katerina-KEA <diamondekku@gmail.com>
2023-06-22 23:07:24 +0300

    added_createGalleryImages.js

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import axios from 'axios';
+import Notiflix from 'notiflix';
+import { createGalleryImages } from './js/createGalleryImages';
+import { toFetchImages } from './js/toFetchImages';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
